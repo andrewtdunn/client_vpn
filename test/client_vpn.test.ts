98f62243eafbf342169d003c18a310fa2327ab95
Andrew Dunn <atd2005@gmail.com>
2025-11-08 00:25:05 -0600

    Initial commit

---
+++ test/client_vpn.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ClientVpn from '../lib/client_vpn-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/client_vpn-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ClientVpn.ClientVpnStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
