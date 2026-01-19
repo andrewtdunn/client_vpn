f78ec383a6774d5dd31f6f29ee7e4ce406c0485c
Andrew Dunn <atd2005@gmail.com>
2026-01-19 12:55:38 -0600

    add vpc

--- lib/client_vpn-stack.ts
+++ lib/client_vpn-stack.ts
@@ -1,9 +1,15 @@
 import * as cdk from "aws-cdk-lib";
+import { IpAddresses, Vpc } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ClientVpnStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new Vpc(this, "TheVPC", {
+      ipAddresses: IpAddresses.cidr("10.0.0.0/16"),
+      vpcName: "DemoVPC",
+    });
   }
 }
