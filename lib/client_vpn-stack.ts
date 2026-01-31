855afc079262641a126e6fd0c9684ee6675099c6
Andrew Dunn <atd2005@gmail.com>
2026-01-31 15:36:38 -0600

    remove split tunnelling

--- lib/client_vpn-stack.ts
+++ lib/client_vpn-stack.ts
@@ -139,7 +139,6 @@ export class ClientVpnStack extends cdk.Stack {
       vpcSubnets: {
         subnetType: SubnetType.PUBLIC,
       },
-      splitTunnel: true,
     });
 
     // Add a 'Name' tag to the endpoint
