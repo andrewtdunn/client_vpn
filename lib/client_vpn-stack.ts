c857065ce914f7c58036a323849fb1a50b5637ab
Andrew Dunn <atd2005@gmail.com>
2026-01-31 15:30:26 -0600

    remove unused

--- lib/client_vpn-stack.ts
+++ lib/client_vpn-stack.ts
@@ -1,7 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { IpAddresses, Vpc } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ClientVpnStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
